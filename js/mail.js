159e8b748709d40495b2ba0893b79d5007a14f0f
Nick Dziubynskyi <nick.dzub@gmail.com>
2022-08-10 13:56:18 +0300

    init proj

---
+++ js/mail.js
@@ -0,0 +1,7 @@
+//! find element on screen
+const form = document.querySelector("#form");
+const taskInput = document.querySelector("#taskInput");
+
+form.addEventListener("submit", function () {
+  console.log("submit");
+});
