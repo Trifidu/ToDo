da6c0c18c8c104342dbf733684f2b6d1e940b968
Nick Dziubynskyi <nick.dzub@gmail.com>
2022-08-10 17:21:20 +0300

    add: render task func

--- js/main.js
+++ js/main.js
@@ -8,27 +8,8 @@ let tasks = [];
 
 if (localStorage.getItem("tasks")) {
   tasks = JSON.parse(localStorage.getItem("tasks"));
+  tasks.forEach((task) => renderTask(task));
 }
-
-tasks.forEach(function (task) {
-  const doneClass = task.done ? "task-title task-title--done" : "task-title";
-
-  const taskHtml = `
-        <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
-					<span class="${doneClass}">${task.text}</span>
-					<div class="task-item__buttons">
-						<button type="button" data-action="done" class="btn-action">
-							<img src="./img/tick.svg" alt="Done" width="18" height="18">
-						</button>
-						<button type="button" data-action="delete" class="btn-action">
-							<img src="./img/cross.svg" alt="Done" width="18" height="18">
-						</button>
-					</div>
-				</li>
-      `;
-
-  tasksList.insertAdjacentHTML("beforeend", taskHtml);
-});
 
 checkEmptyList();
 
@@ -46,23 +27,7 @@ function addTask(e) {
 
   tasks.push(newTask);
 
-  const doneClass = newTask.done ? "task-title task-title--done" : "task-title";
-
-  const taskHtml = `
-        <li id="${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
-					<span class="${doneClass}">${newTask.text}</span>
-					<div class="task-item__buttons">
-						<button type="button" data-action="done" class="btn-action">
-							<img src="./img/tick.svg" alt="Done" width="18" height="18">
-						</button>
-						<button type="button" data-action="delete" class="btn-action">
-							<img src="./img/cross.svg" alt="Done" width="18" height="18">
-						</button>
-					</div>
-				</li>
-      `;
-
-  tasksList.insertAdjacentHTML("beforeend", taskHtml);
+  renderTask(newTask);
 
   taskInput.value = "";
   taskInput.focus();
@@ -133,4 +98,22 @@ function saveToLocalStorage() {
 }
 
 //! render task
-function renderTask() {}
+function renderTask(task) {
+  const doneClass = task.done ? "task-title task-title--done" : "task-title";
+
+  const taskHtml = `
+        <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+					<span class="${doneClass}">${task.text}</span>
+					<div class="task-item__buttons">
+						<button type="button" data-action="done" class="btn-action">
+							<img src="./img/tick.svg" alt="Done" width="18" height="18">
+						</button>
+						<button type="button" data-action="delete" class="btn-action">
+							<img src="./img/cross.svg" alt="Done" width="18" height="18">
+						</button>
+					</div>
+				</li>
+      `;
+
+  tasksList.insertAdjacentHTML("beforeend", taskHtml);
+}
